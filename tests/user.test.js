09a4897bc6b5fdef7a6a34303fc701fe77923792
Patrick <phwang4@wisc.edu>
2021-05-21 18:39:42 -0500

    add tests

---
+++ tests/user.test.js
@@ -0,0 +1,37 @@
+const request = require('supertest')
+const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+    name: 'Mike',
+    email: 'mike@example.com',
+    password: '56what!!'
+}
+
+beforeEach(async () => {
+    await User.deleteMany()
+    await new User(userOne).save()
+})
+
+
+test('Should signup a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Patrick',
+        email: 'patrick.hwang4@example.com',
+        password: 'MyPass777!'
+    }).expect(201)
+})
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({ 
+        email: userOne.email,
+        password: userOne.password,
+    }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: "obviouslyBadEmail@example.com",
+        password: 'password123'
+    }).expect(400)
+})
